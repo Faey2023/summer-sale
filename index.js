b3caedd0aef68e830c10c5be9310f01a360920bf
Fahima <sfahima1388@gmail.com>
2023-08-21 19:17:21 +0600

    added cart

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+let total = 0;
+function product(target) {
+  //   addToCart("Flexible Sofa");
+  const name = target.childNodes[3].innerText;
+  addToCart(name);
+  const priceInt = target.childNodes[5].innerText.split(" ")[0];
+  const price = parseFloat(priceInt);
+  const totalPrice = (total += price);
+  document.getElementById("totalProductPrice").innerText = totalPrice;
+  if (totalPrice > 0) {
+    document.getElementById("btn-purchase").disabled = false;
+  }
+  if (totalPrice >= 200) {
+    document.getElementById("coupon").disabled = false;
+  }
+}
+// reusable function
+function addToCart(productName) {
+  const cart = document.getElementById("cart");
+  const count = cart.childElementCount;
+  const li = document.createElement("li");
+  li.classList.add = ("text-2xl", "text-colBlack", "font-medium");
+  li.innerHTML = `${count + 1}. ${productName}`;
+  cart.appendChild(li);
+}
